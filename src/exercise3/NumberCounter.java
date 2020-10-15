d5ec6ed0bf82434efb54ca94ac2ed094a9114f4c
repl.it user <replituser@example.com>
2020-10-15 06:17:43 +0000

    change

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,8 @@
+package exercise3;
+import Java.util.Scanner;
+public class NumberCounter {
+  public static void main (String[] args) {
+    Scanner input = new Scanner (system.in);
+  
+  }
+}
---
+++ src/exercise3/PrimeNumber.java
@@ -0,0 +1,20 @@
+package exercise3;
+import Java.util.Scanner;
+public class PrimeNumber {
+  public static void main (String[] args) {
+    Scanner input = new Scanner (System.in);
+    int n = input.nextInt;
+    boolean isPrime = true;
+
+    for ( int i = 2; i <= Math.sqrt(n); i++) {
+      if (n%i == 0){
+        isPrime = false;
+      }
+
+      }
+      if else (n%i != 0) {
+        isPrime = true;
+      }
+  }
+  
+}
